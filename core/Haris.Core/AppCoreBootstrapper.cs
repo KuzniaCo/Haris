26e629731356bbfc89bc21bb3605275d53e87cfa
Patryk Acewicz-Hepfner <patryk@kuznia.co>
2015-12-20 21:38:16 +0100

    Groundwork

---
+++ core/Haris.Core/AppCoreBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+using Haris.Core.Modules;
+
+namespace Haris.Core
+{
+	public class AppCoreBootstrapper
+	{
+		public IKernel Kernel { get; private set; }
+
+		void Run()
+		{
+			Kernel = new DefaultKernel();
+			ConfigureKernel();
+		}
+
+		private void ConfigureKernel()
+		{
+			Kernel.Register(Component.For<IEventAggregator>().ImplementedBy<EventAggregator>().LifestyleSingleton());
+			Kernel.Register(
+				Types.FromAssemblyInThisApplication().BasedOn<IHarisModule>().WithService.FromInterface().LifestyleSingleton());
+		}
+	}
+}
---
+++ core/Haris.Core/Modules/IHarisModule.cs
@@ -0,0 +1,7 @@
+﻿namespace Haris.Core.Modules
+{
+	public interface IHarisModule
+	{
+		 
+	}
+}
