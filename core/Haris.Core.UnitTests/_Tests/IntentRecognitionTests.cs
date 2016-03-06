a93037ecf22092d5f85dc804c75fbc969a53c597
Patryk Acewicz-Hepfner <patryk@kuznia.co>
2016-03-06 16:12:18 +0100

    First test + downgrade NUnit

---
+++ core/Haris.Core.UnitTests/_Tests/IntentRecognitionTests.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Haris.Core.UnitTests._Tests
+{
+	[TestFixture]
+	public class IntentRecognitionTests
+	{
+		[SetUp]
+		public void Init()
+		{
+
+		}
+
+		[Test]
+		public void JsonIsProperlyLoaded()
+		{
+			var file = File.ReadAllText("TestData/TurnOnTvResponse.txt");
+
+			object obj = null;
+			Assert.DoesNotThrow(() => obj = JsonConvert.DeserializeObject(file));
+			Assert.IsNotNull(obj);
+		}
+	}
+}
