e6f7ce572e20d1524da29dd07ab3efa339ba24b4
Damian Komorowski <dkomorowski@kuznia.co>
2016-08-04 18:49:16 +0200

    Run simple server on Owin and a simple website http://localhost:88/

--- core/Haris.HostApp/Program.cs
+++ core/Haris.HostApp/Program.cs
@@ -1,22 +1,29 @@
 ﻿using System;
 using System.Threading;
 using Haris.Core;
+using Haris.WebApi;
+using Microsoft.Owin.Hosting;
+using Microsoft.Owin;
 
 namespace Haris.HostApp
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		public static void Main(string[] args)
 		{
 			Console.WriteLine("-----------------------");
 			Console.WriteLine("Press Ctrl+C to quit...");
 			Console.WriteLine("-----------------------");
 			var bootstrapper = new AppCoreBootstrapper();
+			string WebApiUrl = "http://*:88";
 			bootstrapper.Run();
+			var owinHost = WebApp.Start<StartupWebApi>(WebApiUrl);
+
 			var mre = new ManualResetEvent(false);
 			Console.CancelKeyPress += delegate
 			{
 				bootstrapper.Shutdown();
+				owinHost.Dispose();
 				mre.Set();
 			};
 			mre.WaitOne();
---
+++ core/Haris.WebApi/StartupWebApi.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using Haris.WebApi;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(StartupWebApi))]
+namespace Haris.WebApi
+{
+
+	public class StartupWebApi
+	{
+		public void Configuration(IAppBuilder app)
+		{
+
+			app.Run(context =>
+			{
+				context.Response.ContentType = "text/plain";
+
+				string output = string.Format(
+					"I'm running on {0} nFrom assembly {1}",
+					Environment.OSVersion,
+					System.Reflection.Assembly.GetEntryAssembly().FullName
+					);
+
+				return context.Response.WriteAsync(output);
+
+			});
+		}
+	}
+}
