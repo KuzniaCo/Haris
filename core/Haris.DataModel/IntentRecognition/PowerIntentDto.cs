cb44f6e4466bf991bc146f01c97f5a600fa9396f
Patryk Acewicz-Hepfner <patryk@kuznia.co>
2016-09-27 23:45:08 +0200

    Add TargetPinNumber to PowerIntentDto

--- core/Haris.DataModel/IntentRecognition/PowerIntentDto.cs
+++ core/Haris.DataModel/IntentRecognition/PowerIntentDto.cs
@@ -5,5 +5,6 @@ namespace Haris.DataModel.IntentRecognition
 		public IntentLabel IntentLabel { get; set; }
 		public string EntityLabel { get; set; }
 		public string RoomLabel { get; set; }
+		public int? TargetPinNumber { get; set; }
 	}
 }
