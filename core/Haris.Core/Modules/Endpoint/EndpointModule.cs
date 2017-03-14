bdec6685ed57bfed6b9996fb2ca6d0b6ac9e2f79
Sebastian Czarnecki <sczarnecki@pgs-soft.com>
2017-03-14 17:53:17 +0100

    Configure routing

--- core/Haris.Core/Modules/Endpoint/EndpointModule.cs
+++ core/Haris.Core/Modules/Endpoint/EndpointModule.cs
@@ -27,36 +27,36 @@ namespace Haris.Core.Modules.Endpoint
 
         public override void Init()
         {
-            _eventAggregator.Subscribe(this);
-            _baudRate = 115200;
-            _portName = "COM4";
+            //_eventAggregator.Subscribe(this);
+            //_baudRate = 115200;
+            //_portName = "COM4";
 
-            try
-            {
-                _serialPort = new SerialPort(_portName)
-                {
-                    BaudRate = _baudRate,
-                    Parity = Parity.None,
-                    StopBits = StopBits.One,
-                    DataBits = 8
-                };
-                _serialPort.DataReceived += OnDataReceived;
-                _serialPort.Disposed += (sender, args) => { Logger.LogError("Dispose Serial"); };
+            //try
+            //{
+            //    _serialPort = new SerialPort(_portName)
+            //    {
+            //        BaudRate = _baudRate,
+            //        Parity = Parity.None,
+            //        StopBits = StopBits.One,
+            //        DataBits = 8
+            //    };
+            //    _serialPort.DataReceived += OnDataReceived;
+            //    _serialPort.Disposed += (sender, args) => { Logger.LogError("Dispose Serial"); };
 
-                _serialPort.Open();
-                var foo = _serialPort.ReadLine();
+            //    _serialPort.Open();
+            //    //var foo = _serialPort.ReadLine();
 
-                //_eventAggregator.Publish(new ConnectedGatewayEvent("GATEWAY IS READY ON " + _portName));
-                Logger.LogPrompt("Gateway connected");
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError(ex.Message);
-            }
-            Logger.LogPrompt("EndpointModule ready");
-            var rep = new CubeRepository();
-            var cube = rep.GetCube("ad5ft");
-            Logger.LogPrompt(cube.CubeType);
+            //    //_eventAggregator.Publish(new ConnectedGatewayEvent("GATEWAY IS READY ON " + _portName));
+            //    Logger.LogPrompt("Gateway connected");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Logger.LogError(ex.Message);
+            //}
+            //Logger.LogPrompt("EndpointModule ready");
+            //var rep = new CubeRepository();
+            //var cube = rep.GetCube("ad5ft");
+            //Logger.LogPrompt(cube.CubeType);
         }
 
         public override void Handle(AttributedMessageEvent message)
