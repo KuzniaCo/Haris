d892c2087fec4fff0a8636971991998732b37026
Sebastian Czarnecki <sebcza@outlook.com>
2017-06-05 19:32:46 +0200

    Change html formatting

--- core/Cubes/harisTemp.py
+++ core/Cubes/harisTemp.py
@@ -4,8 +4,8 @@ from datetime import datetime
 import requests
 t = ""
 while True:
-    tmpR = requests.get('http://serwer.sebcza.pl:88/api/cube/temperature/aDs6g')
+    tmpR = requests.get('http://haris.sebcza.pl/api/cube/temperature/aDs6g')
     if(t != tmpR.content[1:-1]):
         t = tmpR.content[1:-1]
-        r = requests.post("http://serwer.sebcza.pl:88/api/cube/display/t6G9P", data={'Row': 1, 'Content': t+" C"})
+        r = requests.post("http://haris.sebcza.pl/api/cube/display/t6G9P", data={'Row': 1, 'Content': t+" C"})
         print(r.status_code, r.reason)
